c875b21a2f07d52cbac6de8a9336f53422a626b6
Hareendran M G <hareendranmg@gmail.com>
2023-12-31 18:28:12 +0530

    Refactor signal handling and serial port configuration

--- c_backend/data_dump.c
+++ c_backend/data_dump.c
@@ -12,9 +12,11 @@
 
 volatile sig_atomic_t stop_flag = 0;
 
-void handle_signal(int signum) {
+void handle_signal(int signum)
+{
     printf("Caught signal %d\n", signum);
-    if (signum == SIGINT || signum == SIGTERM) {
+    if (signum == SIGINT || signum == SIGTERM)
+    {
         printf("Stopping...\n");
         stop_flag = 1;
         printf("Stopped\n");
@@ -22,54 +24,32 @@ void handle_signal(int signum) {
     printf("Exiting signal handler\n");
 }
 
-void cleanup(int serial_fd, FILE *outputFile) {
+void cleanup(int serial_fd, FILE *outputFile)
+{
     printf("Cleaning up...\n");
-    if (outputFile) fclose(outputFile);
-    if (serial_fd != -1) close(serial_fd);
+    if (outputFile)
+        fclose(outputFile);
+    if (serial_fd != -1)
+        close(serial_fd);
     printf("Cleaned up\n");
 }
 
-void rename_output_file(const char *outputFileName)
+int open_serial_port(const char *serialPortName)
 {
-    char old_file_name[MAX_FILE_PATH];
-    char time_stamp[MAX_FILE_PATH];
-    char new_file_name[MAX_FILE_PATH];
-
-    time_t now = time(NULL);
-    struct tm *t = localtime(&now);
-    strftime(time_stamp, sizeof(time_stamp), "%Y%m%d_%H%M%S", t);
-    printf("time_stamp: %s\n", time_stamp);
-
-    const char *file_name = strrchr(outputFileName, '/');
-    printf("file_name: %s\n", file_name);
-    const char *file_name_without_path = file_name ? file_name + 1 : outputFileName;
-    printf("file_name_without_path: %s\n", file_name_without_path);
-    const char *file_name_without_ext = strrchr(file_name_without_path, '.');
-    printf("file_name_without_ext: %s\n", file_name_without_ext);
-    const char *file_ext = file_name_without_ext ? file_name_without_ext : "";
-    printf("file_ext: %s\n", file_ext);
-
-    // Construct the new file name
-    snprintf(old_file_name, sizeof(old_file_name), "%.*s_old_%s%s",
-             (file_name_without_ext - file_name_without_path),
-             file_name_without_path, time_stamp, file_ext);
-
-    printf("Renaming %s to %s\n", outputFileName, old_file_name);
-    rename(outputFileName, old_file_name);
-}
-
-int open_serial_port(const char *serialPortName) {
     int serial_fd = open(serialPortName, O_RDWR | O_NOCTTY | O_NONBLOCK);
-    if (serial_fd == -1) {
+    if (serial_fd == -1)
+    {
         perror("Error opening serial port");
     }
     return serial_fd;
 }
 
-void set_serial_parameters(int serial_fd, int baudRate, char parity, int dataBits, int stopBits) {
+void set_serial_parameters(int serial_fd, speed_t baudRate, char parity, int dataBits, int stopBits)
+{
     struct termios tty;
     memset(&tty, 0, sizeof(tty));
-    if (tcgetattr(serial_fd, &tty) != 0) {
+    if (tcgetattr(serial_fd, &tty) != 0)
+    {
         perror("Error getting serial port attributes");
         cleanup(serial_fd, NULL);
         exit(EXIT_FAILURE);
@@ -82,52 +62,74 @@ void set_serial_parameters(int serial_fd, int baudRate, char parity, int dataBit
     tty.c_cflag &= ~CSTOPB;
 
     // Set parity
-    if (parity == 'N') {
+    if (parity == 'N')
+    {
         tty.c_cflag &= ~PARENB; // No parity
-    } else if (parity == 'E') {
+    }
+    else if (parity == 'E')
+    {
         tty.c_cflag |= PARENB;  // Enable parity
         tty.c_cflag &= ~PARODD; // Even parity
-    } else if (parity == 'O') {
+    }
+    else if (parity == 'O')
+    {
         tty.c_cflag |= (PARENB | PARODD); // Odd parity
     }
 
     // Set data bits
     tty.c_cflag &= ~CSIZE;
-    if (dataBits == 8) {
+    if (dataBits == 8)
+    {
         tty.c_cflag |= CS8;
-    } else if (dataBits == 7) {
+    }
+    else if (dataBits == 7)
+    {
         tty.c_cflag |= CS7;
-    } else if (dataBits == 6) {
+    }
+    else if (dataBits == 6)
+    {
         tty.c_cflag |= CS6;
-    } else if (dataBits == 5) {
+    }
+    else if (dataBits == 5)
+    {
         tty.c_cflag |= CS5;
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Invalid number of data bits\n");
         cleanup(serial_fd, NULL);
         exit(EXIT_FAILURE);
     }
 
     // Set stop bits
-    if (stopBits == 1) {
+    if (stopBits == 1)
+    {
         tty.c_cflag &= ~CSTOPB; // 1 stop bit
-    } else if (stopBits == 2) {
+    }
+    else if (stopBits == 2)
+    {
         tty.c_cflag |= CSTOPB; // 2 stop bits
-    } else {
+    }
+    else
+    {
         fprintf(stderr, "Invalid number of stop bits\n");
         cleanup(serial_fd, NULL);
         exit(EXIT_FAILURE);
     }
 
-    if (tcsetattr(serial_fd, TCSANOW, &tty) != 0) {
+    if (tcsetattr(serial_fd, TCSANOW, &tty) != 0)
+    {
         perror("Error setting serial port attributes");
         cleanup(serial_fd, NULL);
         exit(EXIT_FAILURE);
     }
 }
 
-FILE *open_output_file(const char *outputFileName) {
+FILE *open_output_file(const char *outputFileName)
+{
     FILE *outputFile = fopen(outputFileName, "wb");
-    if (!outputFile) {
+    if (!outputFile)
+    {
         perror("Error opening output file");
         cleanup(-1, NULL); // Passing -1 as serial_fd to avoid closing an invalid file descriptor
         exit(EXIT_FAILURE);
@@ -135,7 +137,8 @@ FILE *open_output_file(const char *outputFileName) {
     return outputFile;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     printf("argc: %d\n", argc);
     for (int i = 0; i < argc; i++)
     {
@@ -143,27 +146,25 @@ int main(int argc, char *argv[]) {
     }
     if (argc != 7)
     {
-        printf("Usage: %s <serial_port> <output_file> <baud_rate> <parity> <data_bits> <stop_bits>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <serial_port> <output_file> <baud_rate> <parity> <data_bits> <stop_bits>\n", argv[0]);
         return EXIT_FAILURE;
     }
 
     const char *serialPortName = argv[1];
     const char *outputFileName = argv[2];
-    int baudRate = atoi(argv[3]);
+    speed_t baudRate = atoi(argv[3]);
     char parity = argv[4][0];
     int dataBits = atoi(argv[5]);
     int stopBits = atoi(argv[6]);
 
     // Execute the Python script before opening the serial port
-    char python_command[256];
-    snprintf(python_command, sizeof(python_command), "python3 /home/root/sda/python_backend/serial_workaround.py %s %d %c %d %d", serialPortName, baudRate, parity, dataBits, stopBits);
+    char python_command[MAX_FILE_PATH];
+    snprintf(python_command, sizeof(python_command),
+             "python3 /home/root/sda/python_backend/serial_workaround.py %s %d %c %d %d",
+             serialPortName, (int)baudRate, parity, dataBits, stopBits);
     system(python_command);
 
     int serial_fd = open_serial_port(serialPortName);
-
-    if (access(outputFileName, F_OK) != -1) {
-        rename_output_file(outputFileName);
-    }
 
     FILE *outputFile = open_output_file(outputFileName);
 
@@ -176,17 +177,23 @@ int main(int argc, char *argv[]) {
     printf("Starting...\n");
     char buffer[BUFFER_SIZE];
 
-    while (!stop_flag) {
+    while (!stop_flag)
+    {
         ssize_t bytesRead = read(serial_fd, buffer, sizeof(buffer));
 
-        if (bytesRead > 0) {
+        if (bytesRead > 0)
+        {
             fwrite(buffer, 1, bytesRead, outputFile);
             fflush(outputFile);
-        } else if (bytesRead < 0) {
+        }
+        else if (bytesRead < 0)
+        {
             perror("Error reading from serial port");
             cleanup(serial_fd, outputFile);
             return EXIT_FAILURE;
-        } else {
+        }
+        else
+        {
             // No data read
         }
     }
