18e5ef361aa7dd764f223161a8b90328a476dbf4
Hareendran M G <hareendranmg@gmail.com>
2024-01-09 20:50:28 +0530

    Update serial port configuration and add support for multiple ports

---
+++ testing/command_response.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <gpiod.h>
+#include <time.h>
+// #include <linux/time.h>
+
+#define BILLION 1000000000L
+
+int main()
+{
+    const char *port = "/dev/ttyXR0";
+    int baudrate = B2000000;
+    char buffer[22];
+    char chip[32] = "gpiochip2";
+    unsigned int offset = 8;
+    struct gpiod_line *line = NULL;
+
+    line = gpiod_line_get(chip, offset);
+
+    int fd = open(port, O_RDWR | O_NOCTTY);
+    if (fd == -1)
+    {
+        perror("Error opening serial port");
+        return 1;
+    }
+
+    struct termios serialConfig;
+    if (tcgetattr(fd, &serialConfig) < 0)
+    {
+        perror("Error getting serial port attributes");
+        close(fd);
+        return 1;
+    }
+
+    // Set baud rate
+    cfsetispeed(&serialConfig, baudrate);
+    cfsetospeed(&serialConfig, baudrate);
+
+    // Set character size to 8 bits, enable odd parity, and set stop bits to 1
+    serialConfig.c_cflag |= CS8 | PARENB | PARODD | CSTOPB;
+
+    // Apply the new settings
+    if (tcsetattr(fd, TCSAFLUSH, &serialConfig) < 0)
+    {
+        perror("Error setting serial port attributes");
+        close(fd);
+        return 1;
+    }
+
+    char data_to_write = 0x0F;
+    struct timespec start_time, end_time;
+    long elapsed_time_ns;
+    long elapsed_time_us;
+
+    for (;;)
+    {
+        // clock_gettime(CLOCK_MONOTONIC, &start_time);
+        // gpiod_ctxless_set_value(chip, offset, 1, false, "gpio-toggle", NULL, NULL);
+        // clock_gettime(CLOCK_MONOTONIC, &end_time);
+        // elapsed_time_us = (end_time.tv_sec - start_time.tv_sec) * 1000000 + (end_time.tv_nsec - start_time.tv_nsec) / 1000;
+        // printf("Time taken for GPIO toggle: %ld microseconds\n", elapsed_time_us);
+
+        // Record the start time for serial write
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+        ssize_t bytes_written = write(fd, &data_to_write, 1);
+        ssize_t bytes_read = read(fd, buffer, sizeof(buffer));
+
+
+        if (bytes_read < 0)
+        {
+            perror("Error reading from serial port");
+            close(fd);
+            return 1;
+        }
+
+        // Print the read data
+        // printf("Read data: ");
+        // for (ssize_t i = 0; i < bytes_read; ++i)
+        // {
+        //     printf("%02X ", (unsigned char)buffer[i]);
+        // }
+        // printf("\n");
+
+        // Optional: Add a delay between consecutive writes
+        usleep(1000); // 1000 microseconds delay
+        clock_gettime(CLOCK_MONOTONIC, &end_time);
+        elapsed_time_us = (end_time.tv_sec - start_time.tv_sec) * 1000000 + (end_time.tv_nsec - start_time.tv_nsec) / 1000;
+        printf("Time taken for serial read: %ld microseconds\n", elapsed_time_us);
+    }
+
+    // Close the serial port
+    close(fd);
+
+    return 0;
+}
--- testing/navic.py
+++ testing/navic.py
@@ -1,25 +1,34 @@
 import serial
 import time
 
-port_name = "/dev/ttyXR0"
+port_name1 = "/dev/ttyXR0"
+port_name2 = "/dev/ttyXR7"
 baud_rate = 2000000
 
 # Corrected the variable name
-serial_port = serial.Serial(port_name, baud_rate, parity="O", timeout=1)
-if serial_port:
+serial_port1 = serial.Serial(port_name1, baud_rate, parity="O", timeout=1)
+serial_port2 = serial.Serial(port_name2, baud_rate, parity="O", timeout=0.01)
+
+if serial_port1 and serial_port2:
     try:
         while True:
             command = bytearray()
 
             command.append(0x0F)
-            serial_port.write(command)
+            serial_port1.write(command)
+            serial_port2.write(command)
             # time.sleep(0.1)
 
-            received_data = serial_port.read(22)
-            hex_string = ' '.join([format(byte, '02x') for byte in received_data])
+            received_data1 = serial_port1.read(22)
+            received_data2 = serial_port2.read(22)
 
-            print(f"Received: {hex_string}")
+            hex_string1 = ' '.join([format(byte, '02x') for byte in received_data1])
+            hex_string2 = ' '.join([format(byte, '02x') for byte in received_data2])
+
+            print(f"Received1: {hex_string1}")
+            print(f"Received2: {hex_string2}")
             # time.sleep(0.1)
     finally:
         # Close the serial port in a finally block to ensure it's closed even if an exception occurs
-        serial_port.close()
+        serial_port1.close()
+        serial_port2.close()
