50c4eb83b07c305640ae3badd56284095610425d
Hareendran M G <hareendranmg@gmail.com>
2024-01-09 13:41:57 +0530

    Update serial port settings and communication loop

--- testing/navic.py
+++ testing/navic.py
@@ -1,29 +1,25 @@
 import serial
 import time
 
-port_name = "/dev/ttyXR6"
-baud_rate = 1000000
+port_name = "/dev/ttyXR0"
+baud_rate = 2000000
 
 # Corrected the variable name
-serial_port = serial.Serial(port_name, baud_rate)
-
+serial_port = serial.Serial(port_name, baud_rate, parity="O", timeout=1)
 if serial_port:
     try:
-        Msg1 = bytearray()
-        Msg2 = bytearray()
+        while True:
+            command = bytearray()
+            
+            command.append(0x0F)
+            serial_port.write(command)
+            time.sleep(0.1)
 
-        # Msg1.append(0xA5)
-        # serial_port.write(Msg1)
-        # time.sleep(0.5)        
+            received_data = serial_port.read(10)
+            hex_string = ' '.join([format(byte, '02x') for byte in received_data])
 
-        Msg2.append(0x0F)
-        serial_port.write(Msg2)
-        # time.sleep(0.1)
-
-        received_data = serial_port.read(40)
-        hex_string = ' '.join([format(byte, '02x') for byte in received_data])
-
-        print(f"Received: {hex_string}")
+            print(f"Received: {hex_string}")
+            time.sleep(1)
     finally:
         # Close the serial port in a finally block to ensure it's closed even if an exception occurs
         serial_port.close()
